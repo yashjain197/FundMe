c868a7efd438d7830e37f36b972d0ebcc33711ab
yashjain197 <yashjain19072001@gmail.com>
2024-09-25 09:46:40 +0530

    More update

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -3,4 +3,13 @@ pragma solidity ^0.8.20;
 
 contract FundMe {
 
+    uint256 minimumusd = 5;
+
+    function fund() public payable  {
+        // Have a min $5
+        require(msg.value > 1e18, "did't send enough ETH"); // 1e18
+
+    }
+
+    // function withdraw() public {}
 }
